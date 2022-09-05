addb076e6c936f760d6025feacaeb03e89e49fa4
yu <yu@Ree.localdomain>
2022-09-05 13:03:36 +0800

    correct database settings for test

--- hello.go
+++ hello.go
@@ -3,13 +3,14 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	// Configure the database connection (always check errors)
-	db, err := sql.Open("mysql", "test_mysql:123@(host.docker.internal)/test?parseTime=true")
+	db, err := sql.Open("mysql", "root:123@(127.0.0.1:3306)/test?parseTime=true")
 
 	if err != nil {
 		fmt.Printf("%s when open DB", err)
@@ -23,6 +24,28 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	query := `
+    CREATE TABLE users (
+        id INT AUTO_INCREMENT,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL,
+        created_at DATETIME,
+        PRIMARY KEY (id)
+    );`
+
+	// Executes the SQL query in our database. Check err to ensure there was no error.
+	_, err = db.Exec(query)
+
+	username := "johndoe"
+	password := "secret"
+	createdAt := time.Now()
+
+	result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
+
+	userID, err := result.LastInsertId()
+
+	fmt.Println(userID)
 
 	fmt.Println("Connect successfully")
 }
